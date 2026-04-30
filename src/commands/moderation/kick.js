0d61c088f3e543f090d3b10d0886b7a1c86385fa
moelovestoeslol <moe.sscutiepie@gmail.com>
2026-04-30 18:06:39 +0300

    Update kick.js

--- src/commands/moderation/kick.js
+++ src/commands/moderation/kick.js
@@ -1,1 +1,28 @@
+const { EmbedBuilder, PermissionFlagsBits } = require('discord.js');
 
+module.exports = {
+  name: 'kick',
+  async execute(message, args, client) {
+    if (!message.member.permissions.has(PermissionFlagsBits.KickMembers))
+      return message.reply('❌ You do not have permission to kick members.');
+
+    const target = message.mentions.members.first();
+    if (!target) return message.reply('❌ Please mention a user to kick.');
+    if (!target.kickable) return message.reply('❌ I cannot kick that user.');
+
+    const reason = args.slice(1).join(' ') || 'No reason provided';
+    await target.kick(reason);
+
+    const embed = new EmbedBuilder()
+      .setColor(0xff6600)
+      .setTitle('👟 Member Kicked')
+      .addFields(
+        { name: 'User', value: `${target.user.tag}`, inline: true },
+        { name: 'Moderator', value: `${message.author.tag}`, inline: true },
+        { name: 'Reason', value: reason },
+      )
+      .setTimestamp();
+
+    message.channel.send({ embeds: [embed] });
+  },
+};
