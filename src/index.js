07464e7d50bea718f910f4bc9400f381568eff33
moelovestoeslol <moe.sscutiepie@gmail.com>
2026-04-30 18:04:31 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,1 +1,43 @@
+const { Client, GatewayIntentBits, Collection } = require('discord.js');
+const fs = require('fs');
+const path = require('path');
+require('dotenv').config();
 
+const client = new Client({
+  intents: [
+    GatewayIntentBits.Guilds,
+    GatewayIntentBits.GuildMessages,
+    GatewayIntentBits.MessageContent,
+    GatewayIntentBits.GuildMembers,
+  ],
+});
+
+client.commands = new Collection();
+client.prefix = process.env.PREFIX || '?';
+client.warnings = new Map();
+
+const commandsPath = path.join(__dirname, 'commands');
+const commandFolders = fs.readdirSync(commandsPath);
+
+for (const folder of commandFolders) {
+  const folderPath = path.join(commandsPath, folder);
+  const commandFiles = fs.readdirSync(folderPath).filter(f => f.endsWith('.js'));
+  for (const file of commandFiles) {
+    const command = require(path.join(folderPath, file));
+    client.commands.set(command.name, command);
+  }
+}
+
+const eventsPath = path.join(__dirname, 'events');
+const eventFiles = fs.readdirSync(eventsPath).filter(f => f.endsWith('.js'));
+
+for (const file of eventFiles) {
+  const event = require(path.join(eventsPath, file));
+  if (event.once) {
+    client.once(event.name, (...args) => event.execute(...args, client));
+  } else {
+    client.on(event.name, (...args) => event.execute(...args, client));
+  }
+}
+
+client.login(process.env.TOKEN);
