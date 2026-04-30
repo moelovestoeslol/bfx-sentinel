b8c3a1a5cf655ee1311e2be4643f48c48e3220b2
moelovestoeslol <moe.sscutiepie@gmail.com>
2026-04-30 18:05:28 +0300

    Update ban.js

--- src/commands/moderation/ban.js
+++ src/commands/moderation/ban.js
@@ -1,1 +1,28 @@
+const { EmbedBuilder, PermissionFlagsBits } = require('discord.js');
 
+module.exports = {
+  name: 'ban',
+  async execute(message, args, client) {
+    if (!message.member.permissions.has(PermissionFlagsBits.BanMembers))
+      return message.reply('❌ You do not have permission to ban members.');
+
+    const target = message.mentions.members.first();
+    if (!target) return message.reply('❌ Please mention a user to ban.');
+    if (!target.bannable) return message.reply('❌ I cannot ban that user.');
+
+    const reason = args.slice(1).join(' ') || 'No reason provided';
+    await target.ban({ reason, deleteMessageSeconds: 604800 });
+
+    const embed = new EmbedBuilder()
+      .setColor(0xff0000)
+      .setTitle('🔨 Member Banned')
+      .addFields(
+        { name: 'User', value: `${target.user.tag}`, inline: true },
+        { name: 'Moderator', value: `${message.author.tag}`, inline: true },
+        { name: 'Reason', value: reason },
+      )
+      .setTimestamp();
+
+    message.channel.send({ embeds: [embed] });
+  },
+};
