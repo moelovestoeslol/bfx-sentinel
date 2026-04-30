6135527daf02724e92fdb09c64cbc2a043c105b3
moelovestoeslol <moe.sscutiepie@gmail.com>
2026-04-30 18:04:53 +0300

    Update ready.js

--- src/events/ready.js
+++ src/events/ready.js
@@ -1,1 +1,9 @@
-
+module.exports = {
+  name: 'ready',
+  once: true,
+  execute(client) {
+    console.log(`✅ Logged in as ${client.user.tag}`);
+    console.log(`📌 Prefix: ${client.prefix}`);
+    client.user.setActivity(`${client.prefix}help | Moderating`, { type: 3 });
+  },
+};
